5d20a25a51e8c55f0d114105746ec33375be7493
Brett Taylor <bretttaylor1998@live.co.uk>
2019-05-16 15:46:03 +0100

    Installer now has an ability to copy a ACMH file over.

--- AirportCEO-ModLoader/ACML/AirportCEOModLoader.cs
+++ AirportCEO-ModLoader/ACML/AirportCEOModLoader.cs
@@ -13,13 +13,6 @@ namespace ACML
             Logger.Print($"Started AirportCEOModLoader version {Assembly.GetExecutingAssembly().GetName().Version}");
             Logger.Print("Initialising ModLoader...");
             ModLoader.ModLoader.Initialise();
-
-            Logger.Print($"1: {new ModVersion(25, 25, 25) == new ModVersion(25, 25, 25) == true}");
-            Logger.Print($"2: {new ModVersion(25, 25, 25) != new ModVersion(1, 0, 0) == true}");
-            Logger.Print($"3: {new ModVersion(1, 0, 1) >= new ModVersion(1, 0, 0) == true}");
-            Logger.Print($"4: {new ModVersion(0, 0, 9) <= new ModVersion(1, 0, 0) == true}");
-            Logger.Print($"5: {new ModVersion(1, 0, 0) > new ModVersion(0, 0, 9) == true}");
-            Logger.Print($"6: {new ModVersion(1, 0, 0) < new ModVersion(1, 0, 1) == true}");
         }
     }
 }
--- AirportCEO-ModLoader/ACMLInstaller/Program.cs
+++ AirportCEO-ModLoader/ACMLInstaller/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace ACMLInstaller
 {
@@ -16,6 +19,8 @@ namespace ACMLInstaller
         private static readonly string TARGET_CALL_METHOD = "Entry";
         private static readonly string ORIGINAL_DLL_EXTESION = "_original.dll";
         private static readonly int PATCH_INTO_INSTRUCTION_NUMBER = 2;
+        private static readonly string ACMH_FOLDER_NAME = "ACMH";
+        private static readonly string ACMH_ASSETS_FOLDER_NAME = "ACMHAssets";
 
         public static void Main()
         {
@@ -30,7 +35,12 @@ namespace ACMLInstaller
                     string dll = Path.Combine(directory, DLL_DIRECTORY);
                     try
                     {
-                        Patcher.Patch(dll, ACML_DLL, HARMONY_DLL, PATCH_CALL_TYPE, PATCH_CALL_METHOD, TARGET_CALL_TYPE, TARGET_CALL_METHOD, ORIGINAL_DLL_EXTESION, PATCH_INTO_INSTRUCTION_NUMBER);
+                        Console.WriteLine("Copying AirportCEO Modding Helper.");
+                        CreateOrUpdateACMHDirectory(directory);
+                        Console.WriteLine("Copied AirportCEO Modding Helper.");
+                        Console.WriteLine("");
+                        Console.WriteLine("Patch Starting.");
+                        Patch(dll, ACML_DLL, HARMONY_DLL, PATCH_CALL_TYPE, PATCH_CALL_METHOD, TARGET_CALL_TYPE, TARGET_CALL_METHOD, ORIGINAL_DLL_EXTESION, PATCH_INTO_INSTRUCTION_NUMBER);
                         Console.WriteLine("Patch successful.");
                     }
                     catch (Exception e)
@@ -67,6 +77,78 @@ namespace ACMLInstaller
                 Console.WriteLine($"Did not find the DLL at \"{Path.Combine(directory, DLL_DIRECTORY)}\".");
 
             return result;
+        }
+
+        private static void CreateOrUpdateACMHDirectory(string executableDirectory)
+        {
+            string acmhDirectory = Path.Combine(executableDirectory, ACMH_FOLDER_NAME);
+            Console.WriteLine($"Checking to see if directory exists: {acmhDirectory}");
+            if (Directory.Exists(acmhDirectory) == false)
+            {
+                Directory.CreateDirectory(acmhDirectory);
+            }
+
+            string acmhAssetsDirectory = Path.Combine(acmhDirectory, ACMH_ASSETS_FOLDER_NAME);
+            Console.WriteLine($"Attempting to delete: {acmhAssetsDirectory}");
+            if (Directory.Exists(acmhAssetsDirectory) == true)
+            {
+                foreach (string filePath in Directory.GetFiles(acmhAssetsDirectory))
+                {
+                    Console.WriteLine($"Attempting to delete file: {filePath}");
+                    File.Delete(filePath);
+                }
+                Directory.Delete(acmhAssetsDirectory);
+            }
+
+            Console.WriteLine($"Attempting to create directory {acmhAssetsDirectory}");
+            Directory.CreateDirectory(acmhAssetsDirectory);
+            foreach (string filePath in Directory.GetFiles(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), ACMH_ASSETS_FOLDER_NAME)))
+            {
+                Console.WriteLine($"Attempting to copy file {filePath}");
+                File.Copy(filePath, Path.Combine(acmhAssetsDirectory, Path.GetFileName(filePath)));
+            }
+        }
+
+        public static void Patch(string dllDirectory, string acmlDLL, string harmonyDLL, string patchCallType, string patchCallMethod, string targetCallType, string targetCallMethod,
+            string copyExtension, int PATCH_INTO_INSTRUCTION_NUMBER)
+        {
+            string newDLLDirectory = Path.Combine(Path.GetDirectoryName(dllDirectory), Path.GetFileNameWithoutExtension(dllDirectory)) + copyExtension;
+            string newACMLDLLDirectory = Path.Combine(Path.GetDirectoryName(dllDirectory), Path.GetFileName(acmlDLL));
+            string newHarmonyDLLDirectory = Path.Combine(Path.GetDirectoryName(dllDirectory), Path.GetFileName(harmonyDLL));
+
+            Console.WriteLine($"DLL: {newDLLDirectory}");
+            if (File.Exists(newDLLDirectory) == true)
+            {
+                File.Delete(newDLLDirectory);
+            }
+
+            Console.WriteLine($"ACML: {newACMLDLLDirectory}");
+            if (File.Exists(newACMLDLLDirectory) == true)
+            {
+                File.Delete(newACMLDLLDirectory);
+            }
+
+            Console.WriteLine($"Harmony: {newHarmonyDLLDirectory}");
+            if (File.Exists(newHarmonyDLLDirectory) == true)
+            {
+                File.Delete(newHarmonyDLLDirectory);
+            }
+
+            File.Move(dllDirectory, newDLLDirectory);
+            File.Copy(acmlDLL, newACMLDLLDirectory);
+            File.Copy(harmonyDLL, newHarmonyDLLDirectory);
+
+            AssemblyDefinition patchAssembly = AssemblyDefinition.ReadAssembly(newDLLDirectory);
+            MethodDefinition patchMethod = patchAssembly.MainModule.GetType(patchCallType).Methods.First((x) => x.Name == patchCallMethod);
+            ILProcessor patchIL = patchMethod.Body.GetILProcessor();
+
+            AssemblyDefinition targetAssembly = AssemblyDefinition.ReadAssembly(acmlDLL);
+            MethodReference tagetMethod = targetAssembly.MainModule.GetType(targetCallType).Methods.First(x => x.Name == targetCallMethod);
+
+            patchIL.InsertBefore(patchMethod.Body.Instructions[PATCH_INTO_INSTRUCTION_NUMBER], Instruction.Create(OpCodes.Call, patchMethod.Module.ImportReference(tagetMethod)));
+            patchAssembly.Write(dllDirectory);
+            patchAssembly.Dispose();
+            targetAssembly.Dispose();
         }
     }
 }
