1db37de98dcb270811c047a662d859c7ae6729c7
Brett Taylor <bretttaylor1998@live.co.uk>
2019-06-02 19:15:39 +0100

    Unbounded Camera

---
+++ AirportCEO-ModFramework/ACMF/ModHelper/Camera/UnboundedCamera.cs
@@ -0,0 +1,29 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace ACMF.ModHelper.Camera
+{
+    [HarmonyPatch(typeof(CameraController))]
+    [HarmonyPatch("InitializeCamera")]
+    public class UnboundedCamera
+    {
+        [HarmonyPostfix]
+        public static void Postfix(CameraController __instance)
+        {
+            if (ACMF.Config.UNBOUNDED_CAMERA == false)
+                return;
+
+            __instance.genericMoveCamera.XRangeMin = int.MinValue;
+            __instance.genericMoveCamera.XRangeMax = int.MaxValue;
+            __instance.genericMoveCamera.YRangeMin = int.MinValue;
+            __instance.genericMoveCamera.YRangeMax = int.MaxValue;
+            __instance.genericMoveCamera.ZRangeMin = -2000f;
+            __instance.genericMoveCamera.ZRangeMax = -2f;
+
+            //typeof(CameraController).GetField("minHeight").SetValue(__instance, 2f);
+            //typeof(CameraController).GetField("maxHeight").SetValue(__instance, 2000f);
+            __instance.mainCamera.nearClipPlane = 2f;
+            __instance.mainCamera.farClipPlane = 2100f;
+        }
+    }
+}
--- AirportCEO-ModFramework/ACMF/ModHelper/Config/Impl/ModHelperConfig.cs
+++ AirportCEO-ModFramework/ACMF/ModHelper/Config/Impl/ModHelperConfig.cs
@@ -5,5 +5,6 @@
     {
         public bool ENABLE_INSTANT_LOAD_INTO_SAVE_GAME = false;
         public string INSTANT_LOAD_INTO_SAVE_GAME_FILE = @"C:\Users\MY_USER_NAME\AppData\Roaming\Apoapsis Studios\Airport CEO\Saves\MY_AIRPORT_NAME";
+        public bool UNBOUNDED_CAMERA = false;
     }
 }
